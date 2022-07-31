996390eb6d3b235de47a973e2fd396a747743811
c0nfsd <pathakmilindd@gmail.com>
2022-07-31 12:29:23 +0530

    implemented clock to limit fps to 30 (this makes falling of bird smooth

--- game.py
+++ game.py
@@ -103,16 +103,18 @@ def draw_window(win, bird):
 def main():
     bird = Bird(200, 200)
     win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+    clock = pygame.time.Clock()
 
     run = True
     while run:
+        clock.tick(30)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
-        bird.move()
+       #bird.move()
         draw_window(win, bird)
-        
+
     pygame.quit()
     quit()
 
